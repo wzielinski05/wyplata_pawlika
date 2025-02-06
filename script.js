6ed8bde437a52f729fb969764ccbfef5597f9d76
wzielinski05 <zielinskiwiktor05@gmail.com>
2025-02-06 22:39:21 +0100

    pwa update

--- script.js
+++ script.js
@@ -55,4 +55,21 @@ document.addEventListener("DOMContentLoaded", () => {
   update();
 });
 
+window.addEventListener('load', () => {
+  registerSW();
+});
+
+// Register the Service Worker
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator
+            .serviceWorker
+            .register('serviceworker.js');
+    }
+    catch (e) {
+      console.log('SW registration failed');
+    }
+  }
+}
 
